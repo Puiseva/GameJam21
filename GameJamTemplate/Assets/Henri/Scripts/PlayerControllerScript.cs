1770eac627963e60843baae42b971bbbb4717c4a
Henri <henri.j.heikkila@gmail.com>
2021-11-01 19:08:06 +0200

    1st Commit to Project
    Added player prototype.
    Added Code for movement (horizontal & vertical)

---
+++ GameJamTemplate/Assets/Henri/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float rotateSpeed = 2f;
+
+    public Rigidbody myRB;
+
+    public GameObject myAmmo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //VAIHTOEHTOINEN KOODI LIIKKEELLE
+        //ETEEN & TAAKSE LIIKE
+        float ver = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * ver * speed * Time.deltaTime);
+
+        //VASEN & OIKEA LIIKE
+        float hor = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hor * speed * Time.deltaTime);
+
+        //PELAAJAN ROTAATIO
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        //PELAAJAN LIIKE
+        //float hor = Input.GetAxis("Horizontal");
+        //float ver = Input.GetAxis("Vertical");
+        //myRB.AddForce(new Vector3(hor * speed, 0, ver * speed));
+    }
+}
