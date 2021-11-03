efb65d2825ed27c1f5314d4f6f7dd8f8ccbd190f
Henri <henri.j.heikkila@gmail.com>
2021-11-03 11:39:32 +0200

    6th commit to project
    Latest

--- GameJamTemplate/Assets/Henri/Scripts/DoughnutScript.cs
+++ GameJamTemplate/Assets/Henri/Scripts/DoughnutScript.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class DoughnutScript : MonoBehaviour
 {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "P")
+        {
+            Instantiate(wallExplosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            Instantiate(enemyExplosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
